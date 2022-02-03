1a37c0c44247996eb387dfdd29468e723786d97e
Anthony Edwards <51706026+anthonye93@users.noreply.github.com>
2022-02-03 14:12:43 +0000

    Update m_u_values.py
    added condition to u value loop to keep loop going if u == 0

--- m_u_values.py
+++ m_u_values.py
@@ -30,21 +30,20 @@ total_records = len(gold_standard)
 # For each variable:
 for v in MU_variables:
     print(v)  
-
+    
     # Remove missing rows
     gold_standard.dropna(subset=[v + '_cen'], inplace=True)
     gold_standard.dropna(subset=[v + '_pes'], inplace=True)
+    
+    # counting total number of non-missing probabilistic variables
+    total_records = len(gold_standard)
     
     # Create a column that stores whether or not there is exact agreement for that pair      
     gold_standard[v + "_exact"] = np.where(gold_standard[v + '_pes'] == gold_standard[v + '_cen'], 1, 0)
 
     # Use the sum_col function to create a total number of pairs with exact agreement
     exact = gold_standard[v + "_exact"].sum()
-    
-    # Calculate the total number of non-missing values for this variable
-    # taking the minimum of the length of the pes or cen data sets where the variable isn't missing
-    total_records = min(len(gold_standard[v + '_pes'].notnull()), len(gold_standard[v + '_pes'].notnull()))
-    
+      
     # Divide the total number of exact matches by the total number of records
     value = exact / total_records
 
@@ -59,42 +58,52 @@ print(m_values)
 
 # ----- Sample for calculating U values from full census ----- #
 
-# Randomly sort datasets
-CEN = CEN.sample(frac = 1).reset_index(drop=True)
-PES = PES.sample(frac = 1).reset_index(drop=True)
-
-# Add a ID column to join on
-sample = pd.merge(CEN, PES, left_index = True, right_index = True)
-len(sample) # 492 - length of smaller df
-
 # DataFrame to append to
 u_values = pd.DataFrame([])
 
-# For name variables:
-for v in MU_variables:
+# condition to reset loop if u value is 0
+restart = True
+while restart:
+    
+    # For name variables:
+    for v in MU_variables:
+        
+        # Randomly sort datasets
+        CEN = CEN.sample(frac = 1).reset_index(drop=True)
+        PES = PES.sample(frac = 1).reset_index(drop=True)
 
-    # Remove missing rows
-    sample.dropna(subset=[v + '_cen'], inplace=True)
-    sample.dropna(subset=[v + '_pes'], inplace=True)
+        # Add a ID column to join on
+        sample = pd.merge(CEN, PES, left_index = True, right_index = True)
 
-    # Count
-    total = len(sample)
+        # Remove missing rows
+        sample.dropna(subset=[v + '_cen'], inplace=True)
+        sample.dropna(subset=[v + '_pes'], inplace=True)
 
-    # Agreement count
-    sample[v + "_exact"] = np.where(sample[v + '_pes'] == sample[v + '_cen'], 1, 0)
+        # Count
+        total = len(sample)
 
-    # Create a total number of pairs with exact agreement
-    exact = sample[v + "_exact"].sum()
+        # Agreement count
+        sample[v + "_exact"] = np.where(sample[v + '_pes'] == sample[v + '_cen'], 1, 0)
 
-    # Proportion
-    value = exact / total
+        # Create a total number of pairs with exact agreement
+        exact = sample[v + "_exact"].sum()
 
-    # Append to DataFrame
-    u_values = u_values.append(pd.DataFrame({'variable': v, 'u_value': value}, index=[1]), ignore_index=True)
+        # Proportion
+        value = exact / total
+        
+        # condition to reset loop if u value is 0
+        if value > 0 and v != 'year':
+                      
+            # Append to DataFrame
+            u_values = u_values.append(pd.DataFrame({'variable': v, 'u_value': value}, index=[1]), ignore_index=True)
 
-    # Add DOB U value if needed
-    # u_values = u_values.append(pd.DataFrame(data = ({'u_value': [(1/(365*80)) * 100], 'variable': ['dob']})), ignore_index = True)
-
+            # Add DOB U value if needed
+            # u_values = u_values.append(pd.DataFrame(data = ({'u_value': [(1/(365*80)) * 100], 'variable': ['dob']})), ignore_index = True)
+            
+        else:
+            restart = False
+            break
+            
 # Print
 print(u_values)
 
