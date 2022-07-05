eebb94fd7320ec3241923c0521dc41af26e51fcb
Anthony Edwards <51706026+anthonye93@users.noreply.github.com>
2022-07-05 17:36:52 +0100

    Update Cluster_Function.py

--- Cluster_Function.py
+++ Cluster_Function.py
@@ -7,31 +7,34 @@ import pandas as pd
 def cluster_number(df, id_1, id_2):
 
   # Columns
-  df = df[['id1', 'id2']]
+  df = df[[id_1, id_2]]
 
   # Index column for graphs
   df['index'] = df.index
   
   # Create graph & connected components
   G = nx.from_pandas_edgelist(df, id_1, id_2, 'index')
+  
   # components = nx.connected_component_subgraphs(G)
   components = [G.subgraph(c) for c in nx.connected_components(G)]
   
-  # List to append results to
+  # empty list to append results to
   result = []
 
   # Loop over components, giving each cluster a cluster number
   for i, cc in enumerate(components, 1):
       idx = [dct['index'] for node1, node2, dct in cc.edges(data=True)]
-      group = df.iloc[idx]
+      group = df.loc[idx]
       group['Cluster_Number'] = i
       result.append(group)
     
-  # Convert result to pandas dataframe then spark dataframe
-  result = result.reset_index(drop = True)[[id_1, id_2, 'Cluster_Number']]
+  # Convert result to pandas dataframe
+  result = pd.concat(result).reset_index(drop = True)[[id_1, id_2, 'Cluster_Number']]
   
   # Join cluster number to full data  
-  df = pd.merge(df, result, how = 'left', on = ['id1', 'id2'])
+  df = pd.merge(df, result, how = 'left', on = [id_1, id_2])
   
-  # Return spark df
   return df
+
+
+output = cluster_number(df = , id_1 = '', id_2 = '')
